3b8698538853cdb5f5b65ef1352120ce7703afc4
AaronMillOro <aaron.ibt@gmail.com>
2020-05-02 16:41:09 +0200

    :pencil2: skeleton of the site using django and CSS picnic

---
+++ odontogram/odontogram/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls import include, url
+from django.contrib.staticfiles.urls import static
+
+from . import views
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
+    #url(r'^records/', include('records.urls', namespace='record')),
+]
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
